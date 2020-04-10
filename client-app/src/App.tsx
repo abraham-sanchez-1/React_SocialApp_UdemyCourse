98690e02b435f57678df9e4cb0b02ab8b364adb9
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-10 00:18:15 -0500

    intalled semantics-ui, values now reformatted

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import { Header, Icon, List } from 'semantic-ui-react';
 import './App.css';
 import axios from 'axios';
+
 
 class App extends Component {
   state = {
@@ -24,15 +25,21 @@ class App extends Component {
 
   render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {this.state.values.map((value: any) => (
-            <li key={value.id}>{value.name}</li>
+    <div >
+      <Header as='h2'>
+        <Icon name='plug' />
+      <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+      {this.state.values.map((value: any) => (
+              <List.Item key={value.id}>{value.name}</List.Item>
           ))}
+  
+  </List>
+        <ul>
+          
         </ul>
-      </header>
+
     </div>
   );
   }
