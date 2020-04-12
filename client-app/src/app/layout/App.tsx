3cf3b0a6b1ecb6115b04fdcabc9905e057ae1b9e
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-12 01:09:31 -0500

    Added IActivity to create strong types to work

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import { Header, Icon, List } from 'semantic-ui-react';
-import './App.css';
 import axios from 'axios';
+import { IActivity } from '../models/activity';
+
+interface IState {
+  activities: IActivity[]
+}
 
 
-class App extends Component {
-  state = {
-    values: []
+class App extends Component<{}, IState> {
+  readonly state: IState = {
+    activities: []
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
     .then((response) => {
       this.setState({
-        values: response.data
+        activities: response.data
       })
     })
     // this.setState({
@@ -30,8 +34,8 @@ class App extends Component {
       <Header.Content>Reactivities</Header.Content>
       </Header>
       <List>
-      {this.state.values.map((value: any) => (
-              <List.Item key={value.id}>{value.name}</List.Item>
+      {this.state.activities.map((activity) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
   
   </List>
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,8 @@
+export interface IActivity {
+    id: string;
+    title: string;
+    description: string;
+    date: Date;
+    city: string;
+    venue: string;
+}
