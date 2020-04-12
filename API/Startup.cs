55dbb97e305506bef367961375fa2d7e46ee8a3a
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-11 19:42:33 -0500

    Delete command added, tested in postman

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,9 @@
-using System;
 using Persistence;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using MediatR;
 using Application.Activities;
