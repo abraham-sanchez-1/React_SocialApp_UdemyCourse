08da3600a20304ab444688856e606c73737a226a
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-13 20:02:26 -0500

    Edit form now only displays after edit button in details is clicked

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -4,9 +4,10 @@ import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activity: IActivity;
+  setEditMode: (editMode: boolean) => void;
 }
 
-export const ActivityDetails: React.FC<IProps> = ({ activity }) => {
+export const ActivityDetails: React.FC<IProps> = ({ activity, setEditMode}) => {
   return (
     <Card>
       <Image
@@ -23,7 +24,7 @@ export const ActivityDetails: React.FC<IProps> = ({ activity }) => {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
-          <Button basic color="blue" content="Edit"></Button>
+          <Button onClick={() => setEditMode(true)} basic color="blue" content="Edit"></Button>
           <Button basic color="grey" content="Cancel"></Button>
         </Button.Group>
       </Card.Content>
