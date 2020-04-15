60c3506969b27cb7ec1dee1eb2a39caf382fa763
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-14 21:59:52 -0500

    Isolated loading indicator to just the delete button

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { ActivityList } from "./ActivityList";
@@ -14,8 +14,9 @@ interface IProps {
   setSelectedActivity: (activity: IActivity | null) => void;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
-  deleteActivity: (id: string) => void;
+  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
   submitting: boolean;
+  target: string;
 }
 
 export const ActivityDashboard: React.FC<IProps> = ({
@@ -29,6 +30,7 @@ export const ActivityDashboard: React.FC<IProps> = ({
   editActivity,
   deleteActivity,
   submitting,
+  target
 }) => {
   return (
     <Grid>
@@ -38,6 +40,7 @@ export const ActivityDashboard: React.FC<IProps> = ({
           selectActivity={selectActivity}
           deleteActivity={deleteActivity}
           submitting={submitting}
+          target={target}
         />
       </Grid.Column>
       <Grid.Column width={6}>
