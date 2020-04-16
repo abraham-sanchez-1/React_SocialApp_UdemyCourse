2e9eb2264fe87b4b96c6bbb08c0de44e3204656c
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-16 00:15:53 -0500

    Cleaned up code, removed unused imports and methods no longer in use

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,5 @@
-import React, { SyntheticEvent, useContext } from "react";
+import React, { useContext } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
 import { observer } from "mobx-react-lite";
 import ActivityStore from "../../../app/stores/activityStore"
 
