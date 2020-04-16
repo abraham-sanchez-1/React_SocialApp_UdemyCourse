7618130d24cefa10c08237706ae3d626920ba664
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-15 23:13:15 -0500

    Refactored code to use computed properties of mobx, activityRegistry to be used to hold all activities

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,51 +1,57 @@
-import { observable, action } from "mobx";
+import { observable, action, computed } from "mobx";
 import { createContext } from "react";
 import { IActivity } from "../models/activity";
 import agent from "../api/agent";
 
 class ActivityStore {
+  @observable activityRegistry = new Map();
   @observable activities: IActivity[] = [];
   @observable selectedActivity: IActivity | undefined;
   @observable loadingInitial = false;
   @observable editMode = false;
   @observable submitting = false;
 
+  @computed get activitiesByDate() {
+    return Array.from(this.activityRegistry.values()).sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
+
   @action loadActivities = async () => {
     this.loadingInitial = true;
     try {
-        const activities = await  agent.Activities.list();
-        activities.forEach((activity) => {
-            activity.date = activity.date.split(".")[0];
-            this.activities.push(activity);
-          });
-          this.loadingInitial = false;
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split(".")[0];
+        this.activityRegistry.set(activity.id, activity);
+      });
+      this.loadingInitial = false;
     } catch (error) {
-        console.log(error);
-        this.loadingInitial = false;
+      console.log(error);
+      this.loadingInitial = false;
     }
   };
 
   @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
-    try{
+    try {
       await agent.Activities.create(activity);
-      this.activities.push(activity);
+      this.activityRegistry.set(activity.id, activity);
       this.editMode = false;
       this.submitting = false;
-    } catch(error) {
+    } catch (error) {
       this.submitting = false;
       console.log(error);
-
     }
   };
 
-  @action openCreateForm= () => {
+  @action openCreateForm = () => {
     this.editMode = true;
     this.selectedActivity = undefined;
-  }
+  };
 
   @action selectActivity = (id: string) => {
-    this.selectedActivity = this.activities.find((a) => a.id === id);
+    this.selectedActivity = this.activityRegistry.get(id);
     this.editMode = false;
   };
 }
