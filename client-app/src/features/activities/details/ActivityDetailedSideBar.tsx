c84f40a393e9f81c75f59525ed3cd648e7f27c80
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-17 16:43:58 -0500

    four new components created and scaffolded, activitydetailedheader is near its completed state in terms of styling

---
+++ client-app/src/features/activities/details/ActivityDetailedSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActivityDetailedSideBar = () => {
+    return (
+        <div>
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
+
+export default ActivityDetailedSideBar
