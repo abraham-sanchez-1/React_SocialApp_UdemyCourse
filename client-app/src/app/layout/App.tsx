23de29a894ae3101ddd025cb10aa22c09bb423ba
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-23 22:54:18 -0500

    Created NotFound component, will be displayed when activities are null. Next: Throw errors in client as well

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,10 +3,11 @@ import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
-import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import { Route, withRouter, RouteComponentProps, Switch } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import NotFound from "./NotFound";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
 
@@ -19,6 +20,8 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
+              <Switch>
+
               <Route exact path='/activities' component={ActivityDashboard} />
               <Route path='/activities/:id' component={ActivityDetails} />
               <Route
@@ -26,6 +29,10 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                 path={['/createActivity', '/manage/:id']}
                 component={ActivityForm}
               />
+
+              <Route component={NotFound}/>
+              </Switch>
+              
             </Container>
           </Fragment>
         )}
