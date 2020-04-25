32ae0241707c8ab5d4f79ef53f7f564026d2acaf
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-24 21:11:24 -0500

    installed react-toastify, will serve as place for error notification

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -26,6 +26,9 @@ namespace Application.Activities
 
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
+
+                throw new Exception("Computer says no");
+                
                 var activity = await _context.Activities.FindAsync(request.Id);
 
                 if (activity == null)
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,12 +7,14 @@ import { Route, withRouter, RouteComponentProps, Switch } from "react-router-dom
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import {ToastContainer} from 'react-toastify';
 import NotFound from "./NotFound";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
 
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' />
       <Route exact path="/" component={HomePage} />
       <Route
         path={'/(.+)'}
