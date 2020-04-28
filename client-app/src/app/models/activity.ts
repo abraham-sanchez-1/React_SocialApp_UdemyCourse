68fd73d98bdfa5da75e8bac202adf800d1f1f80e
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-27 22:05:03 -0500

    older versions of semantics-ui and final form installed for compatibility, working on getting date picker to fully function, working on freeing compile errors

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -2,7 +2,7 @@ export interface IActivity {
     id: string;
     title: string;
     description: string;
-    date: string;
+    date: Date | null;
     city: string;
     venue: string;
     category: string;
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,6 +10,7 @@ import TextInput from "../../../app/common/form/TextInput";
 import TextAreaInput from "../../../app/common/form/TextAreaInput";
 import { SelectInput } from "../../../app/common/form/SelectInput";
 import { category } from "../../../app/common/options/categoryOptions";
+import { DateInput } from "../../../app/common/form/DateInput";
 
 interface DetailParams {
   id: string;
@@ -36,7 +37,7 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
     title: "",
     category: "",
     description: "",
-    date: "",
+    date: null,
     city: "",
     venue: "",
   });
@@ -99,7 +100,7 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
             name="date"
             placeholder="Date"
             value={activity.date}
-            component={TextInput}
+            component={DateInput}
           />
           <Field
             name="city"
