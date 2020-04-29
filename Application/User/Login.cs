f72c139dad340789ae5e9f07769a8be9f8b57358
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-29 18:37:45 -0500

    New object created to send back when getting user, sends back only displayname, token, username, and image. tested in postman

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -12,7 +12,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -28,7 +28,7 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
@@ -39,7 +39,7 @@ namespace Application.User
 
             }
 
-            public async Task<AppUser> Handle(Query request,
+            public async Task<User> Handle(Query request,
                 CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
@@ -54,7 +54,12 @@ namespace Application.User
                 if (result.Succeeded)
                 {
                     // TODO: generate token
-                    return user;
+                    return new User{
+                        DisplayName = user.DisplayName,
+                        Token = "This will be a token",
+                        UserName = user.UserName,
+                        Image = null
+                    };
                 }
                 else 
                 {
