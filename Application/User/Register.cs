35a0b5c3f49f899cb071881fe76143c61db09791
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-04-30 01:33:29 -0500

    Register class and functionality have been added.

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -1,7 +1,15 @@
 using System;
+using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.User
@@ -10,24 +18,73 @@ namespace Application.User
     {
         public class Command : IRequest<User>
         {
+            public string DisplayName { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
 
         }
-        public class Handler : IRequestHandler<Command>
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+                RuleFor(x => x.UserName).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+        public class Handler : IRequestHandler<Command, User>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly UserManager<AppUser> _userManager;
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
             {
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
             {
 
-                //handler logic
-                var success = await _context.SaveChangesAsync() > 0;
+                if (await _context.Users.Where(x => x.Email == request.Email).AnyAsync())
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new { Email = "Email already exists" });
+                }
+                if (await _context.Users.Where(x => x.UserName == request.UserName).AnyAsync())
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new { UserName = "Username already exists" });
+                }
 
-                if (success) return Unit.Value;
-                throw new Exception("Problem saving changes");
+                var user = new AppUser
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.UserName
+                };
+
+
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded)
+                {
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        UserName = user.UserName,
+                        Image = null
+                    };
+                }
+                else
+                {
+                    throw new Exception("Problem creating user");
+                }
+
 
 
             }
