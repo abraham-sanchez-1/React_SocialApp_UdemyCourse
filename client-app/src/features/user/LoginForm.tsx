4b1f369cccb367c48bb2cc2a629df360c04c0a2d
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-01 14:06:51 -0500

    new component added to handle error messaging to user on client side

--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -6,6 +6,7 @@ import { RootStoreContext } from "../../app/stores/rootStore";
 import { IUserFormValues } from "../../app/models/users";
 import { FORM_ERROR } from "final-form";
 import { combineValidators, isRequired } from "revalidate";
+import ErrorMessage from "../../app/common/form/ErrorMessage";
 
 const validate = combineValidators({
   email: isRequired("email"),
@@ -32,7 +33,7 @@ const LoginForm = () => {
         pristine,
         dirtySinceLastSubmit
       }) => (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} error>
           <Header as='h2' content='Login to Reactivities' color='teal' textAlign='center' />
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
@@ -42,7 +43,7 @@ const LoginForm = () => {
             type="password"
           />
           {submitError && !dirtySinceLastSubmit && (
-            <Label color="red" basic content={submitError.statusText} />
+           <ErrorMessage error={submitError} text='Invalid email or password' />
           )}
           <Button
             disabled={invalid && !dirtySinceLastSubmit || pristine}
