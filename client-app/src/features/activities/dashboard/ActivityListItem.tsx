848aa1182a9412e0c098f539c05807a9b8398c62
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-04 20:16:40 -0500

    Attendees now dynamically appear on main activities list component, popup reveals attendee name

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -2,28 +2,30 @@ import React from "react";
 import { Item, Button, Segment, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { IActivity } from "../../../app/models/activity";
-import {format} from 'date-fns';
+import { format } from "date-fns";
+import ActivityListItemAttendees from "./ActivityListItemAttendees";
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
-
   return (
     <Segment.Group>
       <Segment>
-          <Item.Group>
-        <Item>
-          <Item.Image size="tiny" circular src="/assets/user.png" />
-          <Item.Content>
-            <Item.Header as="a">{activity.title}</Item.Header>
-            <Item.Description>Hosted by Bob</Item.Description>
-          </Item.Content>
-        </Item>
+        <Item.Group>
+          <Item>
+            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Description>Hosted by Bob</Item.Description>
+            </Item.Content>
+          </Item>
         </Item.Group>
       </Segment>
       <Segment>
-        <Icon name="clock" /> {format(activity.date, 'h:mm a')}
+        <Icon name="clock" /> {format(activity.date, "h:mm a")}
         <Icon name="marker" /> {activity.venue}, {activity.city}
       </Segment>
-      <Segment secondary>Attendees will go here</Segment>
+      <Segment secondary>
+        <ActivityListItemAttendees attendees={activity.attendees}/>
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
