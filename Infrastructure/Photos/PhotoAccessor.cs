479084a98f3ade5c46d4d6a9c982164376e4ac27
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-06 18:45:11 -0500

    Photo command handler has been added

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,3 +1,4 @@
+using System;
 using Application.Interfaces;
 using Application.Photos;
 using CloudinaryDotNet;
@@ -37,6 +38,13 @@ namespace Infrastructure.Photos
                     uploadResult = _cloudinary.Upload(uploadParams);
                 }
             }
+
+            if(uploadResult.Error != null)
+            {
+                throw new Exception(uploadResult.Error.Message);
+            }
+
+
             return new PhotoUploadResult
             {
                PublicId = uploadResult.PublicId,
