66917772cd37a9ebc8e917e1bcddaf0b76cd0cb6
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-07 18:04:34 -0500

    Added api side creation of profile bio, can now be changed and persists. Tested in postman

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
 using Application.Profiles;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ProfilesController :BaseController
+    public class ProfilesController : BaseController
     {
         [HttpGet("{username}")]
         public async Task<ActionResult<Profile>> Get(string username)
@@ -12,6 +13,10 @@ namespace API.Controllers
             return await Mediator.Send(new Details.Query{Username = username});
         }
 
-
+        [HttpPut]
+        public async Task<ActionResult<Unit>> Edit(Edit.Command command)
+        {
+            return await Mediator.Send(command);
+        }
     }
 }
