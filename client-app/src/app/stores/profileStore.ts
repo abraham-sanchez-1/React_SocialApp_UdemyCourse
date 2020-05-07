09cd36a52e859d6bacd825bc513b822c6de914eb
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-07 18:22:26 -0500

    API and client side changes complete for editing bio in profile as well as displayname

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -96,5 +96,20 @@ this.loading= false;
         this.loading = false;
       })
     }
-  }
+  };
+
+
+  @action updateProfile = async (profile: Partial<IProfile>) => {
+    try {
+        await agent.Profiles.updateProfile(profile);
+        runInAction(() => {
+            if (profile.displayName !== this.rootStore.userStore.user!.displayName) {
+                this.rootStore.userStore.user!.displayName = profile.displayName!;
+            }
+            this.profile = {...this.profile!, ...profile}
+        })
+    } catch (error) {
+        toast.error('Problem updating profile')
+    }
+}
 }
