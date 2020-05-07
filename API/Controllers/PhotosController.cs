017cbde9c99b60107bff71833ccd454754cad7d2
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-06 20:30:49 -0500

    Delete functionality for photos has been added on the application/api side

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Application.Photos;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -8,9 +9,16 @@ namespace API.Controllers
     public class PhotosController : BaseController
     {
         [HttpPost]
-        public async Task<ActionResult<Photo>> Add([FromForm]Add.Command command)
+        public async Task<ActionResult<Photo>> Add([FromForm] Add.Command command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(string id)
+        {
+            return await Mediator.Send(new Delete.Command { Id = id });
+
         }
     }
 }
