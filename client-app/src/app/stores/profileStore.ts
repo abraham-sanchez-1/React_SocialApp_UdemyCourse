7b06f471c228524d8991d742f4627841c1b15860
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-07 00:16:02 -0500

    Initial aesthetic of photos tab in profile page added, will move next to implement functionality of buttons

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,5 +1,5 @@
 import { RootStore } from "./rootStore";
-import { observable, action, runInAction } from "mobx";
+import { observable, action, runInAction, computed } from "mobx";
 import agent from "../api/agent";
 import { IProfile } from "../models/profile";
 
@@ -11,6 +11,14 @@ export default class ProfileStore {
 
   @observable profile: IProfile | null = null;
   @observable loadingProfile = true;
+
+  @computed get isCurrentUser() {
+    if(this.rootStore.userStore.user && this.profile) {
+      return this.rootStore.userStore.user.username === this.profile.username;
+    } else {
+      return false;
+    }
+  }
 
   @action loadProfile = async (username: string) => {
       this.loadingProfile = true;
@@ -27,4 +35,6 @@ export default class ProfileStore {
       console.log(error);
     }
   };
+
+
 }
