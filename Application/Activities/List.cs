b0c69f8e16a9f1dbd56342ed3acb3e7a0003fd66
abraham.sanchezolea@gmail.com <sanchez95san@gmail.com>
2020-05-12 16:12:43 -0500

    Added parameters and variables to be used in get request for activities, these will be used for filtering when request is made. Tested successfully in postman

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -7,6 +7,8 @@ using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using System;
+using Application.Interfaces;
 
 namespace Application.Activities
 {
@@ -19,22 +21,30 @@ namespace Application.Activities
         }
         public class Query : IRequest<ActivitiesEnvelope>
         {
-            public Query(int? limit, int? offset)
+            public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
             {
                 Limit = limit;
                 Offset = offset;
+                IsGoing = isGoing;
+                IsHost = isHost;
+                StartDate = startDate ?? DateTime.Now;
 
             }
             public int? Limit { get; set; }
             public int? Offset { get; set; }
+            public bool IsGoing { get; set; }
+            public bool IsHost { get; set; }
+            public DateTime? StartDate { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
 
@@ -43,10 +53,27 @@ namespace Application.Activities
             public async Task<ActivitiesEnvelope> Handle(Query request,
                 CancellationToken cancellationToken)
             {
-                var queryable = _context.Activities.AsQueryable();
+                var queryable = _context.Activities
+                .Where(x => x.Date >= request.StartDate)
+                .OrderBy(x => x.Date)
+                .AsQueryable();
+
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable
+                    .Where(x => x.UserActivities
+                    .Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+                }
+
+                if(request.IsHost && !request.IsGoing)
+                {
+                     queryable = queryable
+                     .Where(x => x.UserActivities
+                     .Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
+                }
 
                 var activities = await queryable
-                .Skip(request.Offset?? 0)
+                .Skip(request.Offset ?? 0)
                 .Take(request.Limit ?? 3)
                 .ToListAsync();
 
@@ -55,7 +82,7 @@ namespace Application.Activities
                     Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
                     ActivityCount = queryable.Count()
                 };
-                 
+
             }
         }
     }
